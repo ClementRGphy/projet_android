d8e381f4d119934ec59e3d57e145f90ca6dab695
tmarius <thomas.marius@hotmail.fr>
2018-10-14 15:34:47 +0200

    Menu bar / Background

--- app/src/main/java/com/example/clem/marius_risso/MainActivity.java
+++ app/src/main/java/com/example/clem/marius_risso/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.clem.marius_risso;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 // Test
@@ -9,5 +13,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.website : {
+                Uri uri = Uri.parse("http://www.univ-poitiers.fr/");
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+                return true;
+            }
+
+
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
